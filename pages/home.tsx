181c4d777dfc147d772006c60c36e073b6b60ee8
Gustavo Eklund <gustavo04616@gmail.com>
2021-02-11 18:06:29 -0300

    feat: added Home page

---
+++ pages/home.tsx
@@ -0,0 +1,9 @@
+import Layout from '@/components/Layout'
+
+export default function Home(): JSX.Element {
+    return (
+        <Layout title="Presencial">
+            <h1>Home</h1>
+        </Layout>
+    )
+}
