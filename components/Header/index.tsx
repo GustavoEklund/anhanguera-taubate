2041fb8f467d8d5b659c36e6e3c9eb7b37460b6d
Gustavo Eklund <gustavo04616@gmail.com>
2021-02-11 18:06:21 -0300

    feat: added Header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,62 @@
+import Image from 'next/image'
+import Styles from './styles.module.scss'
+import Button from '@/components/Button'
+import TelegramIcon from '@/components/Icons/TelegramIcon'
+import InstagramIcon from "@/components/Icons/InstagramIcon";
+import FacebookIcon from "@/components/Icons/FacebookIcon";
+import WhatsAppIcon from "@/components/Icons/WhatsAppIcon";
+
+export default function Header(): JSX.Element {
+    return (
+        <header className={Styles.header}>
+            <nav>
+                <Image
+                    src="/assets/anhanguera_logo_alt.svg"
+                    height={40}
+                    width={140}
+                />
+                <div className={Styles.buttonsWrapper}>
+                    <Button startIcon={<Image src="/assets/bell_icon.svg" height={20} width={20} alt="Importante" />}>
+                        Importante
+                    </Button>
+                    <Button startIcon={<Image src="/assets/youtube_icon.svg" height={20} width={20} alt="Tutoriais"/>}>
+                        Tutoriais
+                    </Button>
+                    <Button startIcon={<Image src="/assets/envelope_icon.svg" height={20} width={20} alt="Contatos"/>}>
+                        Contatos
+                    </Button>
+                </div>
+                <div className={Styles.socialMediaWrapper}>
+                    <a
+                        href="https://api.whatsapp.com/send/?phone=551236344146"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        <InstagramIcon />
+                    </a>
+                    <a
+                        href="https://api.whatsapp.com/send/?phone=551236344146"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        <FacebookIcon />
+                    </a>
+                    <a
+                        href="https://api.whatsapp.com/send/?phone=551236344146"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        <WhatsAppIcon />
+                    </a>
+                    <a
+                        href="https://api.whatsapp.com/send/?phone=551236344146"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        <TelegramIcon />
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
+}
