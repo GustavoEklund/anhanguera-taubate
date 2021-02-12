1a8aa516abb8b7776201a6e2bdad32836f468f42
Gustavo Eklund <gustavo04616@gmail.com>
2021-02-12 18:14:13 -0300

    feat: add home page

--- pages/inicio/index.tsx
+++ pages/inicio/index.tsx
@@ -1,9 +1,53 @@
+import Image from 'next/image'
 import Layout from '@/components/Layout'
+import Button from '@/components/Button'
+import Styles from './styles.module.scss'
 
-export default function Index(): JSX.Element {
+type Post = {
+    id: number
+    title: string
+    subtitle: string
+    mainImage: string
+    description?: object
+    link: null | string
+}
+
+type Props = {
+    posts: Post[]
+}
+
+export default function Index(props: Props): JSX.Element {
     return (
-        <Layout title="Presencial">
-            <h1>Home</h1>
+        <Layout title="Presencial" footerDisabled>
+            <div className={Styles.container}>
+                {props.posts.map((post): JSX.Element => (
+                    <div
+                        className={Styles.card}
+                        key={post.id}
+                        data-title={post.title}
+                        onClick={() => post.link ? window.open(post.link) : null}
+                    >
+                        <img
+                            src={`http://public.essencialavida.com/images/${post.mainImage}`}
+                            width={300}
+                            height={452}
+                            alt={post.title}
+                        />
+                        {((Object.keys(post?.description || {}).length > 0) || post.link) && (
+                            <Button>Saiba mais</Button>
+                        )}
+                    </div>
+                ))}
+            </div>
         </Layout>
     )
+}
+
+export async function getStaticProps(context) {
+    const response = await fetch(`http://public.essencialavida.com/data/presential_posts.json`)
+    const posts: Post[] = await response.json()
+
+    return {
+        props: { posts }
+    }
 }
