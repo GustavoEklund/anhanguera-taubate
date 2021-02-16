e23167cc67beaf897204161bc7ecbc223f527d95
Gustavo Eklund <gustavo04616@gmail.com>
2021-02-16 17:49:10 -0300

    feat: added Modal behaviour and useModal hook

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,10 +1,33 @@
 import Styles from './styles.module.scss'
+import React from "react";
 
-export default function Modal(): JSX.Element {
+interface ModalProps {
+    title?: React.ReactNode | React.ReactNode[]
+    children?: React.ReactNode | React.ReactNode[]
+    footer?: React.ReactNode | React.ReactNode[]
+    isShowing: boolean
+    toggle: () => void
+}
+
+export default function Modal({ title, children, footer, isShowing, toggle }: ModalProps): JSX.Element {
     return (
-        <div aria-modal className={Styles.modalOverlay}>
-            <div className={Styles.modalWrapper}>
-                <header><button>&times;</button></header>
+        <div
+            aria-modal
+            className={[Styles.modalOverlay, Styles[isShowing ? 'modalOpen' : 'modalClosed']].join(' ')}
+            onClick={toggle}
+        >
+            <div
+                className={Styles.modalWrapper}
+                onClick={(event) => event.stopPropagation()}
+            >
+                <header>
+                    <span>{title}</span>
+                    <button type="button" onClick={toggle}>&times;</button>
+                </header>
+                <main>{children}</main>
+                {!footer && (
+                    <footer>{footer}</footer>
+                )}
             </div>
         </div>
     )
---
+++ hooks/useModal.ts
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+type ModalSate = [isShowing: boolean, toggle: () => void]
+
+export default function useModal(): ModalSate {
+    const [isShowing, setIsShowing] = useState<boolean>(false)
+    const toggle = (): void => setIsShowing((prevState: boolean): boolean => !prevState)
+    return [ isShowing, toggle ]
+}
