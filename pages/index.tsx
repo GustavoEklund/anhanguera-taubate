0cdbe29fc6afc8d844efae5ecd94a5179462a6b4
Gustavo Eklund <gustavo04616@gmail.com>
2021-02-16 17:49:51 -0300

    feat: added Modal with map to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,12 @@ import AnhangueraSquareLogo from '@/components/Icons/AnhangueraSquareLogo'
 import HeroCornerShape from '@/components/HeroCornerShape'
 import SocialMediaLinks from '@/components/SocialMediaLinks'
 import Button from '@/components/Button'
+import Modal from '@/components/Modal'
+import useModal from "@/hooks/useModal";
 
 export default function Landing(): JSX.Element {
     const { windowWidth } = useWindowSize()
+    const [isShowingModal, toggleModal] = useModal()
     const isDesktopWindowSize = (): boolean => windowWidth > 1000
 
     return (
@@ -34,7 +37,12 @@ export default function Landing(): JSX.Element {
                     {!isDesktopWindowSize() && (
                         <div className={Styles.location}>
                             <SocialMediaLinks />
-                            <Button starticon={<MapMarker />}>Nossa localização</Button>
+                            <Button
+                                starticon={<MapMarker />}
+                                onClick={toggleModal}
+                            >
+                                Nossa localização
+                            </Button>
                         </div>
                     )}
                     <div className={Styles.footerTitle}>
@@ -44,7 +52,12 @@ export default function Landing(): JSX.Element {
                         {isDesktopWindowSize() && (
                             <div className={Styles.location}>
                                 <SocialMediaLinks />
-                                <Button starticon={<MapMarker />}>Nossa localização</Button>
+                                <Button
+                                    starticon={<MapMarker />}
+                                    onClick={toggleModal}
+                                >
+                                    Nossa localização
+                                </Button>
                             </div>
                         )}
                         <div className={Styles.button}>
@@ -68,18 +81,24 @@ export default function Landing(): JSX.Element {
                         </div>
                     </div>
                 )}
-                {/*<div>*/}
-                {/*    <iframe*/}
-                {/*        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1835.978429168541!2d-45.57337711640215!3d-23.025356198118967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ccf90761a2a5cb%3A0xa734b5e1da168ef9!2sAnhanguera%20Educacional!5e0!3m2!1spt-BR!2sbr!4v1613481641193!5m2!1spt-BR!2sbr"*/}
-                {/*        width="600"*/}
-                {/*        height="450"*/}
-                {/*        frameBorder="0"*/}
-                {/*        style={{ border: 0 }}*/}
-                {/*        allowFullScreen=""*/}
-                {/*        aria-hidden="false"*/}
-                {/*        tabIndex="0"*/}
-                {/*    />*/}
-                {/*</div>*/}
+                <Modal
+                    isShowing={isShowingModal}
+                    toggle={toggleModal}
+                    title="Localização da Faculdade Anhanguera de Taubaté"
+                >
+                    <div className={Styles.mapWrapper}>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1835.978429168541!2d-45.57337711640215!3d-23.025356198118967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ccf90761a2a5cb%3A0xa734b5e1da168ef9!2sAnhanguera%20Educacional!5e0!3m2!1spt-BR!2sbr!4v1613481641193!5m2!1spt-BR!2sbr"
+                            width="100%"
+                            height="100%"
+                            frameBorder="0"
+                            style={{ border: 0 }}
+                            allowFullScreen
+                            aria-hidden="false"
+                            tabIndex={0}
+                        />
+                    </div>
+                </Modal>
             </div>
         </Layout>
     )
