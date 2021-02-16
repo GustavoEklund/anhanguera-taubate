a89f4d0c363a2d83ddb0cce3f0f7c30ba499fca5
Gustavo Eklund <gustavo04616@gmail.com>
2021-02-16 17:47:41 -0300

    feat: added endIcon to button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -3,15 +3,17 @@ import Styles from './styles.module.scss'
 
 interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
     starticon?: React.ReactNode
+    endicon?: React.ReactNode
     variant?: 'default' |  'contained' | 'outlined'
 }
 
 export default function Button(props: ButtonProps): JSX.Element {
-    const { children, starticon } = props
+    const { children, starticon, endicon, ...otherProps } = props
     return (
-        <button {...props} className={[Styles.button, Styles[`${props.variant || 'default'}Button`]].join(' ')}>
-            { starticon && <span className={Styles.icon}>{starticon}</span>}
+        <button {...otherProps} className={[Styles.button, Styles[`${props.variant || 'default'}Button`]].join(' ')}>
+            { starticon && <span className={Styles.startIcon}>{starticon}</span>}
             <span>{children}</span>
+            { endicon && <span className={Styles.endIcon}>{endicon}</span>}
         </button>
     )
 }
