6f46827209c7dbd22569b2439288cd344ff2662e
Gustavo Eklund <gustavo04616@gmail.com>
2021-02-18 17:15:14 -0300

    refactor: make HamburgerMenu use useModality

--- components/HamburgerMenu/index.tsx
+++ components/HamburgerMenu/index.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 import Styles from './styles.module.scss'
 import BarsIcon from '@/components/Icons/BarsIcon'
-import { useState } from 'react'
 import BellIcon from '@/components/Icons/BellIcon'
 import YoutubeIcon from '@/components/Icons/YoutubeIcon'
 import EnvelopeIcon from '@/components/Icons/EnvelopeIcon'
 import SocialMediaLinks from '@/components/SocialMediaLinks'
+import useModality from '@/hooks/useModality'
 
 export default function HamburgerMenu(): JSX.Element {
+  const router = useRouter()
   const [isShowing, setIsShowing] = useState<boolean>(false)
+  const { modality } = useModality()
 
   function handleToggle(): void {
     setIsShowing((prevState) => !prevState)
@@ -22,20 +26,20 @@ export default function HamburgerMenu(): JSX.Element {
       <div className={[Styles.menuOverlay, Styles[getMenuState()]].join(' ')} />
       <SocialMediaLinks className={[Styles.menuMediaLinks, Styles[getMenuState()]].join(' ')} />
       <ul className={[Styles.menuItems, Styles[getMenuState()]].join(' ')}>
-        <li>
+        <li onClick={() => router.push(`/inicio?modalidade=${modality}`)} tabIndex={-1}>
           <span>Importante</span>
           <BellIcon width={20} height={20} fill="#333" />
         </li>
-        <li>
+        <li onClick={() => router.push(`/tutoriais?modalidade=${modality}`)} tabIndex={-1}>
           <span>Tutoriais</span>
           <YoutubeIcon width={20} height={20} fill="#333" />
         </li>
-        <li>
+        <li onClick={() => router.push(`/contatos?modalidade=${modality}`)} tabIndex={-1}>
           <span>Contatos</span>
           <EnvelopeIcon width={20} height={20} fill="#333" />
         </li>
       </ul>
-      <button className={Styles.hamburgerButton} onClick={handleToggle}>
+      <button className={Styles.hamburgerButton} onClick={handleToggle} title="Menu">
         <BarsIcon />
       </button>
     </>
