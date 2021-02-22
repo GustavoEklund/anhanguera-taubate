a36c67a666c5a4f8a36e343d3e0bb4b9ebe6c149
Gustavo Eklund <gustavo04616@gmail.com>
2021-02-22 12:59:32 -0300

    feat: add fullscreen feature to image

--- pages/informacao/components/Image.tsx
+++ pages/informacao/components/Image.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
 import Styles from './styles.module.scss'
+import ExpandIcon from '@/components/Icons/ExpandIcon'
+import TimesIcon from '@/components/Icons/TimesIcon'
 
 type ImageProps = {
   src: string
@@ -6,5 +9,46 @@ type ImageProps = {
 }
 
 export default function Image({ src, title }: ImageProps): JSX.Element {
-  return <img src={src} className={Styles.image} alt={title || 'Imagem'} />
+  const [isFullScreen, setIsFullScreen] = useState<boolean>(false)
+
+  function handleFullScreen(): void {
+    setIsFullScreen(true)
+  }
+
+  function handleExitFullScreen(): void {
+    setIsFullScreen(false)
+  }
+
+  return (
+    <figure className={Styles.imageWrapper}>
+      <img
+        src={src}
+        className={[Styles.image, Styles[isFullScreen ? 'imageFullScreen' : 'imageNormal']].join(
+          ' '
+        )}
+        alt={title || 'Imagem'}
+        title={title || 'Imagem'}
+        tabIndex={-1}
+      />
+      <button onClick={handleFullScreen}>
+        <ExpandIcon />
+      </button>
+      <div className={Styles.imageBackground} />
+      <figcaption>{title || 'Imagem'}</figcaption>
+      <div
+        className={[
+          Styles.imageOverlay,
+          Styles[isFullScreen ? 'imageOverlayShowing' : 'imageOverlayNormal']
+        ].join(' ')}
+        onClick={handleExitFullScreen}
+      >
+        <header>
+          <span>{title || 'Imagem'}</span>
+          <button className={Styles.closeIcon}>
+            <TimesIcon />
+          </button>
+        </header>
+      </div>
+    </figure>
+  )
 }
