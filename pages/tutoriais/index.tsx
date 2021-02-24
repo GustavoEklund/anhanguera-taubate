4964ec5073d47e191dd5572b75558a588a54a6d8
Gustavo Eklund <gustavo04616@gmail.com>
2021-02-24 13:34:48 -0300

    feat: add markdown load to Tutorials

--- pages/tutoriais/index.tsx
+++ pages/tutoriais/index.tsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
+import { GetStaticProps, NextPage } from 'next'
+import gfm from 'remark-gfm'
+import ReactMarkdown from 'react-markdown'
 import Styles from './styles.module.scss'
 import Layout from '@/components/Layout'
 import useModality from '@/hooks/useModality'
 import AccordionDropDown from '@/components/AccordionDropDown'
 
-export default function Tutoriais(): JSX.Element {
+function flatten(text: any, child: any): any {
+  return typeof child === 'string'
+    ? text + child
+    : React.Children.toArray(child.props.children).reduce(flatten, text)
+}
+
+type HeadingRendererProps = {
+  children: ReactElement
+  level: '1' | '2' | '3' | '4' | '5' | '6'
+}
+
+function HeadingRenderer(props: HeadingRendererProps): ReactElement {
+  const children = React.Children.toArray(props.children)
+  const text = children.reduce(flatten, '')
+  const slug = text
+    .toLowerCase()
+    .replace(/\s[—-]\s/g, '-')
+    .replace(/\W/g, '-')
+  return React.createElement('h' + props.level, { id: slug }, props.children)
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const baseUrl = 'https://public.essencialavida.com/data'
+  const response = await fetch(`${baseUrl}/presential_tutorial.md`)
+  const tutorial = await response.text()
+
+  return {
+    props: { tutorial },
+    revalidate: 1800
+  }
+}
+
+type Props = {
+  tutorial: string
+}
+
+const Tutoriais: NextPage<Props> = ({ tutorial }: Props) => {
   const { modality } = useModality()
 
   return (
@@ -13,11 +52,12 @@ export default function Tutoriais(): JSX.Element {
         <div className={Styles.tutorialsWrapper}>
           <aside className={Styles.sideMenu}>
             <AccordionDropDown startOpen title="01 - Primeiros passos">
-              {[
-                <a key={1} href="#primeiros_passos" className={Styles.dropDownItem}>
-                  01.1 - Acesso ao portal do aluno
-                </a>
-              ]}
+              <a href="#01-1-acesso-ao-portal-do-aluno" className={Styles.dropDownItem}>
+                01.1 - Acesso ao portal do aluno
+              </a>
+              <a href="#01-2-primeiros-passos-no-portal-do-aluno" className={Styles.dropDownItem}>
+                01.2 - Primeiros passos no Portal do Aluno
+              </a>
             </AccordionDropDown>
           </aside>
           <div className={Styles.containerWrapper}>
@@ -25,20 +65,14 @@ export default function Tutoriais(): JSX.Element {
               <div className={Styles.titleWrapper}>
                 <h1>Tutoriais presencial</h1>
               </div>
-              <div className={Styles.titleWrapper} id="primeiros_passos">
-                <h2>01 - Primeiros Passos</h2>
-              </div>
-              <div className={Styles.tutorial}>
-                <iframe
-                  src="https://www.youtube.com/embed/XKFQJgYc_gk"
-                  frameBorder={0}
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                  title="Acesso ao portal do aluno"
-                />
-                <figcaption>01 - Tutorial de acesso ao portal do aluno</figcaption>
-              </div>
             </div>
+            <ReactMarkdown
+              className={Styles.markdownContent}
+              plugins={[gfm]}
+              renderers={{ heading: HeadingRenderer }}
+            >
+              {tutorial}
+            </ReactMarkdown>
           </div>
         </div>
       )}
@@ -61,3 +95,5 @@ export default function Tutoriais(): JSX.Element {
     </Layout>
   )
 }
+
+export default Tutoriais
